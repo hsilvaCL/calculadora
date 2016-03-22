6fec1c05866036b89f1f62b6e5fd9118a2a9e450
cetecom <cetecom@LC08L1303>
2016-03-21 21:02:51 -0300

    cambio final de la clase

--- index.php
+++ index.php
@@ -7,11 +7,12 @@
         <meta charset="UTF-8">
         <title></title>
     </head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <body>
        
-        <form action="calculosarit.php" method="POST">
+        <form action="calculosarit.php"  onsubmit="return calculo()" method="POST">
             Valor A<br>
-            <input type="text" name="val_a">
+            <input type="text" name="val_a" id="val_a">
             <br>
             <select name="operacion">
                 <option value="+">+</option>
@@ -22,8 +23,25 @@
               </select>
             <br>
             Valor B:<br>
-            <input type="text" name="val_b">
+            <input type="text" name="val_b" id ="val_b">
             <input type="submit" value="Enviar">
         </form>
+        <script type="text/javascript">
+            function calculo(){
+                var a = $("#val_a").val();
+                if (a==""){
+                    alert("Defina valor de A");
+                    return false;
+                }
+                var b = $("#val_b").val();
+                if (b==""){
+                    alert("Defina valor de B");
+                    return false;
+                }
+                return true;
+            }
+            
+        </script>
     </body>
+    
 </html>
